53ba1a0c768caab208c45d375f8f142119dde3fb
ArsacMohammed <arsacskasha@gmail.com>
2023-09-06 18:09:52 +0530

    added dependencies injection and Spring Autowried content in firstProject ,notes added

---
+++ firstProject/src/main/java/com/Arsac/first/Laptop.java
@@ -0,0 +1,10 @@
+package com.Arsac.first;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+	public void compile(){
+		System.out.println("Compiling");
+	}
+}
