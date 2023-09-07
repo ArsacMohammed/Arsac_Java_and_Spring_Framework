ac37de98798ab6b039eced253f40110c106c2a33
ArsacMohammed <arsacskasha@gmail.com>
2023-09-07 19:16:46 +0530

    Model and ModelMap

--- demoSpringBoot/src/main/java/com/Arsac/springMVCBoot/HomeController.java
+++ demoSpringBoot/src/main/java/com/Arsac/springMVCBoot/HomeController.java
@@ -1,10 +1,12 @@
 package com.Arsac.springMVCBoot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -19,17 +21,18 @@ public class HomeController {
 		System.out.println("helo");
 		return "index";
 	}
-// for security reasons , the two jsp files are moved to views file and application properties is added to config it
-// prefix and suffix is added for files to be found 
-// for applicaiton properties visit-https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html#application-properties.web.spring.mvc.view.prefix
+// if we dont want to use ModelAndView , we can use Model (which create simple object or ModelMap (for Storing like a map).
 	@RequestMapping("add")
-	public ModelAndView add (@RequestParam("num1") int i,@RequestParam("num2") int j) {
+	public String add (@RequestParam("num1") int i,@RequestParam("num2") int j,Model m){
 		int num3=i+j;
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("num3",num3);
-	    mv.setViewName("result"); // give the name of the jsp file  where we print the view .
-		return mv;
+
+//		ModelAndView mv = new ModelAndView("result");  // this also works
+//		mv.addObject("num3",num3);
+//	    mv.setViewName("result"); 
+		m.addAttribute("num3",num3);
+		return "result";
 	}
+	
 }
 
 
