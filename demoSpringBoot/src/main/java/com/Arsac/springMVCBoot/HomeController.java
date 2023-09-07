118bb163d8851166a92c1265b629ae4d480e1570
ArsacMohammed <arsacskasha@gmail.com>
2023-09-07 18:23:12 +0530

    added prefix and suffix for security concerns

--- demoSpringBoot/src/main/java/com/Arsac/springMVCBoot/HomeController.java
+++ demoSpringBoot/src/main/java/com/Arsac/springMVCBoot/HomeController.java
@@ -17,15 +17,17 @@ public class HomeController {
 
 	public String home() {
 		System.out.println("helo");
-		return "index.jsp";
+		return "index";
 	}
-//to remove httpsession we can use ModelAndView class.
+// for security reasons , the two jsp files are moved to views file and application properties is added to config it
+// prefix and suffix is added for files to be found 
+// for applicaiton properties visit-https://docs.spring.io/spring-boot/docs/current/reference/html/application-properties.html#application-properties.web.spring.mvc.view.prefix
 	@RequestMapping("add")
 	public ModelAndView add (@RequestParam("num1") int i,@RequestParam("num2") int j) {
 		int num3=i+j;
 		ModelAndView mv = new ModelAndView();
 		mv.addObject("num3",num3);
-	    mv.setViewName("result.jsp"); // give the name of the jsp file  where we print the view .
+	    mv.setViewName("result"); // give the name of the jsp file  where we print the view .
 		return mv;
 	}
 }
