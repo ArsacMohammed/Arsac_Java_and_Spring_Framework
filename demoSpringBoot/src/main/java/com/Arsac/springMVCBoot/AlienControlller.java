164d7bb2d336472a43ecb9efc3e7603fdfa66b2f
ArsacMohammed <arsacskasha@gmail.com>
2023-09-09 10:47:12 +0530

    pathvariable picking by one particular id

--- demoSpringBoot/src/main/java/com/Arsac/springMVCBoot/AlienControlller.java
+++ demoSpringBoot/src/main/java/com/Arsac/springMVCBoot/AlienControlller.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.Arsac.springMVCBoot.model.Alien;
@@ -19,6 +20,13 @@ public class AlienControlller {
 		List<Alien> aliens= repo.findAll();
 		return aliens;   // 
 	}
+	@GetMapping("aliens/{aid}")
+	@ResponseBody
+	public Alien getalienById(@PathVariable("aid") int aid) {
+		Alien a = repo.findById(aid).orElse(new Alien(0,"")); // orElse is present to ensure  if the id is not present in the database we will return (0,");
+		return a;
+	}
+	
 }
 /// here the java object are converted into jason files by using inbuilt jackson jar file  in maven dependencies .
 // the viceversa is also possible like recoverting the jason file into java object in the server .
