1f8798edca283e2ac7709d82790a2de57cde261e
ArsacMohammed <arsacskasha@gmail.com>
2023-09-09 11:09:27 +0530

    @PostMapping ,send data to the server ,with postman

--- demoSpringBoot/src/main/java/com/Arsac/springMVCBoot/AlienControlller.java
+++ demoSpringBoot/src/main/java/com/Arsac/springMVCBoot/AlienControlller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,12 +22,16 @@ public class AlienControlller {
 		List<Alien> aliens= repo.findAll();
 		return aliens;   // 
 	}
-	@GetMapping("aliens/{aid}")
+	@GetMapping("alien/{aid}")
 	
 	public Alien getalienById(@PathVariable("aid") int aid) {
 		Alien a = repo.findById(aid).orElse(new Alien(0,"")); // orElse is present to ensure  if the id is not present in the database we will return (0,");
 		return a;
 	}
-	
+	@PostMapping("alien")// note get is to fetch the information from the server and post is to send the information to the server.
+	public Alien addAlien(Alien a) {
+		repo.save(a);
+		return a;
+	}// instead of using forms to get the input , we use postname change the option to post ,in the param area ,enter the key value as aid or aname and in the value area enter the respective values.
 }
 /// since  we using @ responsebody for each method for converting, we can change the @Controller to @RestController and remove @Response body above each method that is responsible for converting
