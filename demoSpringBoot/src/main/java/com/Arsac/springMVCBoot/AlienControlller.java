37771589d03b41c07b1d567a42aaab5b39becc2b
ArsacMohammed <arsacskasha@gmail.com>
2023-09-09 10:52:59 +0530

    @RestController is added and @ResponseBody is removed

--- demoSpringBoot/src/main/java/com/Arsac/springMVCBoot/AlienControlller.java
+++ demoSpringBoot/src/main/java/com/Arsac/springMVCBoot/AlienControlller.java
@@ -7,26 +7,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.Arsac.springMVCBoot.model.Alien;
-@Controller
+@RestController
 public class AlienControlller {
 	@Autowired
 	AlienRepo repo;
 	
 	@GetMapping("aliens")
-	@ResponseBody  /// this send data rather than sending jsp like in homecontroller (configured as prefix and suffix in the appilication properties.
+	 /// this send data rather than sending jsp like in homecontroller (configured as prefix and suffix in the appilication properties.
 	public List<Alien> getAliens() {
 		List<Alien> aliens= repo.findAll();
 		return aliens;   // 
 	}
 	@GetMapping("aliens/{aid}")
-	@ResponseBody
+	
 	public Alien getalienById(@PathVariable("aid") int aid) {
 		Alien a = repo.findById(aid).orElse(new Alien(0,"")); // orElse is present to ensure  if the id is not present in the database we will return (0,");
 		return a;
 	}
 	
 }
-/// here the java object are converted into jason files by using inbuilt jackson jar file  in maven dependencies .
-// the viceversa is also possible like recoverting the jason file into java object in the server .
+/// since  we using @ responsebody for each method for converting, we can change the @Controller to @RestController and remove @Response body above each method that is responsible for converting
