35c280df6ec35b76127e4048e333e9e5b0913ac3
ArsacMohammed <arsacskasha@gmail.com>
2023-09-09 18:47:42 +0530

    spring security login page added

---
+++ SecureApp/src/test/java/com/Arsac/springMVCBoot/Secureapp/SecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Arsac.springMVCBoot.Secureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
