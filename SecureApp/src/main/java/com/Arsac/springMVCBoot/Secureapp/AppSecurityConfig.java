06be996c5c8117432f61afb62efea105e6a9dd70
ArsacMohammed <arsacskasha@gmail.com>
2023-09-10 09:55:35 +0530

    added BcrpytEncoder and plain text will not work

--- SecureApp/src/main/java/com/Arsac/springMVCBoot/Secureapp/AppSecurityConfig.java
+++ SecureApp/src/main/java/com/Arsac/springMVCBoot/Secureapp/AppSecurityConfig.java
@@ -9,12 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @Configuration
@@ -26,10 +21,10 @@ public class AppSecurityConfig {
     public AuthenticationProvider authProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
         return provider;
     }
 
 
 }
-///did not understand completely need revisit this topic but managed to complete the code somehow
+/// see to it that plian password like web from database will  not work
