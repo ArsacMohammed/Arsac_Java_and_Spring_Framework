88fd0a632440ea32087b248dd842b1533e95bb2a
ArsacMohammed <arsacskasha@gmail.com>
2023-09-10 15:26:59 +0530

    initial commit for Quiz App

---
+++ QuizApp/src/main/java/com/Arsac/springMVCBoot/QuizApp/QuizAppApplication.java
@@ -0,0 +1,13 @@
+package com.Arsac.springMVCBoot.QuizApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Starting Quiz App from now ...................................................
+@SpringBootApplication
+public class QuizAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppApplication.class, args);
+	}
+
+}
